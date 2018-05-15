ed8455b49d99dd99d18bb90ddf406acfe861ee13
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 19:53:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,24 +24,24 @@ class Owner
     return "I am a #{species}."
   end
 
-  # def buy_fish(name)
-  #   fish = Fish.new(name)
-  #   @pets[:fishes] << fish
-  # end
-  #
-  # def buy_cat(name)
-  #   cat = Cat.new(name)
-  #   @pets.each do |species|
-  #     species[1] << cat
-  #   end
-  # end
-  #
-  # def buy_dog(name)
-  #   dog = Dog.new(name)
-  #   @pets.each do |species|
-  #     species[2] << dog
-  #   end
-  # end
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets.each do |species|
+      species[1] << cat
+    end
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets.each do |species|
+      species[2] << dog
+    end
+  end
 
   # def walk_dogs
   #   @pets.Dog.mood = "happy"
