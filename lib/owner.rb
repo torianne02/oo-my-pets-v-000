f0cf40b704fe101423df6071db3c4e874462e355
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 18:52:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -11,18 +11,4 @@ class Owner
       species[0] << fish
     end
   end
-
-  def buy_cat(name)
-    cat = Cat.new(name)
-    @pets.each do |species|
-      species[1] << cat
-    end
-  end
-
-  def buy_dog(name)
-    dog = Dog.new(name)
-    @pets.each do |species|
-      species[2] << dog
-    end
-  end
 end
