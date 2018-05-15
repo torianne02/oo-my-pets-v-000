d7eb2ea992fecaf0e84703f73328de4027ebdb93
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 18:53:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,33 @@
 class Owner
   attr_accessor :pets
+  @@all = []
 
   def initialize(pets)
     @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def self.all
+    @@all
   end
 
   def buy_fish(name)
     fish = Fish.new(name)
     @pets.each do |species|
       species[0] << fish
+    end
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets.each do |species|
+      species[1] << cat
+    end
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets.each do |species|
+      species[2] << dog
     end
   end
 end
