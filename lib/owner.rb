418a35af4386d6cee0ff519fc9a1ec20d6d1b8c1
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 18:50:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,17 +5,17 @@ class Owner
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
-  def buy_cat(name)
-    cat = Cat.new(name)
-    @pets.each do |specie_array|
-      specie_array[1] << cat
-    end
-  end
-
   def buy_fish(name)
     fish = Fish.new(name)
     @pets.each do |specie_array|
       specie_array[0] << fish
+    end
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets.each do |specie_array|
+      specie_array[1] << cat
     end
   end
 
