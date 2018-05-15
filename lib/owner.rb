240861d006253623f74ee2cb133c7b39df67143b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 20:11:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,7 @@ class Owner
   @@all = []
 
   def initialize(pets)
-    @pets = {:fishes => [], :cats => [], :dogs => []}
+    @pets = {fishes: [], cats: [], dogs: []}
     @species = "human"
     @@all << self
   end
