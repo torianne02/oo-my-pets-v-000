885debe459b9a1c3901f10d426fdb0ef8262e748
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 18:35:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,12 @@
 class Owner
   attr_accessor :pets
-  
+
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets.cats << cat 
   end
 end
