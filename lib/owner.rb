43d9ce8db2e5851764ff72f611ada0b70b07b117
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 20:06:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,11 +44,11 @@ class Owner
   def play_with_cats
     @pets[:cats].each {|cat| cat.mood = "happy"}
   end
-  
+
   def feed_fish
     @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
-  
+
   # def list_pets
   #   @pets.each do |species|
   #     species.each {|pet_name| puts pet_name(name)}
